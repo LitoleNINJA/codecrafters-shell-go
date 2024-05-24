8ba704544a4b2e0eef93cd8f6e84bbc1b8b9af68
Ritwik <sritwik03@gmail.com>
2024-05-24 23:04:49 +0530

    task 2

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,12 +4,24 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		_, _ = reader.ReadString('\n')
+		cmd, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return
+		}
+
+		cmd = strings.ReplaceAll(cmd, "\n", "")
+		handleCommand(cmd)
 	}
+}
+
+func handleCommand(cmd string) {
+	fmt.Printf("%s: command not found\n", cmd)
 }
