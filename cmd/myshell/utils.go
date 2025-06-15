bdcb195e6aede6bf143152444fed7642c1310bd5
LitoleNINJA <sritwik2@gmail.com>
2025-06-15 12:44:54 +0530

    codecrafters submit [skip ci]

--- cmd/myshell/utils.go
+++ cmd/myshell/utils.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"strings"
 )
@@ -13,50 +12,66 @@ func parseInput(input string) (string, []string) {
 	}
 
 	var args []string
-    var current strings.Builder
-    inQuotes := false
-    quoteChar := byte(0)
+	var current strings.Builder
+	inSingleQuotes, inDoubleQuotes := false, false
 
-    for i := 0; i < len(input); i++ {
-        char := input[i]
+	for i := 0; i < len(input); i++ {
+		char := input[i]
 
-        if !inQuotes && (char == '\'' || char == '"') {
-            inQuotes = true
-            quoteChar = char
-        } else if inQuotes && char == quoteChar {
-            inQuotes = false
-            quoteChar = 0
-        } else if !inQuotes && char == ' ' {
-            if current.Len() > 0 {
-                args = append(args, current.String())
-                current.Reset()
-            }
+		switch {
+		case char == '\'' && !inDoubleQuotes:
+			inSingleQuotes = !inSingleQuotes
+		case char == '"' && !inSingleQuotes:
+			inDoubleQuotes = !inDoubleQuotes
+		case char == ' ' && !inSingleQuotes && !inDoubleQuotes:
+			if current.Len() > 0 {
+				args = append(args, current.String())
+				current.Reset()
+			}
+			// Skip multiple spaces
+			for i+1 < len(input) && input[i+1] == ' ' {
+				i++
+			}
+		case char == '\\' && !inSingleQuotes:
+			if nextPos, ok := handleEscape(input, i, &current, inDoubleQuotes); ok {
+				i = nextPos
+				continue
+			} else {
+				return "", nil
+			}
+		default:
+			current.WriteByte(char)
+		}
+	}
 
-            for i+1 < len(input) && input[i+1] == ' ' {
-                i++
-            }
-        } else if !inQuotes && char == '\\' {
-            if i+1 < len(input) {
-                i++
-                current.WriteByte(input[i])
-            } else {
-                fmt.Println("error: trailing backslash in input")
-                return "", nil
-            }
-        } else {
-            current.WriteByte(char)
-        }
-    }
+	if current.Len() > 0 {
+		args = append(args, current.String())
+	}
 
-    if current.Len() > 0 {
-        args = append(args, current.String())
-    }
+	if len(args) == 0 {
+		return "", nil
+	}
 
-    if len(args) == 0 {
-        return "", nil
-    }
+	return args[0], args[1:]
+}
 
-    return args[0], args[1:]
+func handleEscape(input string, i int, current *strings.Builder, inDoubleQuotes bool) (int, bool) {
+	if i+1 >= len(input) {
+		return i, false
+	}
+
+	nextChar := input[i+1]
+
+	if inDoubleQuotes {
+		// In double quotes, only escape ", \, and $
+		// Backslashes preceding characters without a special meaning are left unmodified
+		if !(nextChar == '"' || nextChar == '\\' || nextChar == '$') {
+			current.WriteByte('\\')
+		}
+	}
+
+	current.WriteByte(nextChar)
+	return i + 1, true
 }
 
 func isOnPath(cmd string) (string, bool) {
